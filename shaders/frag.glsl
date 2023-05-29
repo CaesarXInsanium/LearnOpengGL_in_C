4b6867e5533b5ce66a88707adfd4652cb345b17f
Ceasar X Insanium <jesusramos.official@gmail.com>
2023-05-28 21:39:18 -0500

    banane bus

--- shaders/frag.glsl
+++ shaders/frag.glsl
@@ -7,9 +7,13 @@ in vec2 TexCoord;
 layout (location = 0) uniform sampler2D ourTexture00;
 layout (location = 1) uniform sampler2D ourTexture01;
 
+vec2 texture_coord_flip(vec2 coord){
+  return vec2(coord.x, abs(1 - coord.y));
+}
+
 void main(){
   // flip the texture coordinates over the y axis
-  vec2 new_tex = vec2(TexCoord.x, abs(1 - TexCoord.y));
+  vec2 new_tex = texture_coord_flip(TexCoord);
 
   FragColor = mix(texture(ourTexture00, new_tex),
                   texture(ourTexture01, new_tex), 0.5);
